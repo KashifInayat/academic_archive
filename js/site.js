4ffa3451501fc8f09c403e5971277c5b728fd2ce
KashifInayat <kinayat@bsc.es>
2024-08-26 02:11:24 +0200

    update

--- js/site.js
+++ js/site.js
@@ -21,7 +21,7 @@ var webURL = "https://kashifinayat.github.io/index.html";
 
 var about = {
    "name": "Kashif Inayat",
-    "heading": "SENIOR DESIGN ENGINEER @ MICROARCHITECTURE DESIGN AND IMPLEMENTATION (CORE)",
+    "heading": "SENIOR DESIGN ENGINEER @ BSC, CPU TEAM",
     "profles": [
 	
 		{
